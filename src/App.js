4c0e439751a626e167ddda9a3ac30b58a8df7a81
KelsCree <kelseycreehan@gmail.com>
2020-10-20 19:08:54 -0600

    Nav bar setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MainContent from './Components/MainContent';
 import { Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <MainContent/>
-    </div>
-  );
-}
+class App extends Component {
 
+    state = {
+        isLoggedIn: false,
+        applications: [],
+        interviews: [],
+        reccomendations: []
+    }
+
+    componentDidMount() {
+      fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then()
+    }
+
+    render() {
+    return (
+      <div className="App">
+        <MainContent/>
+      </div>
+    );
+  }
+}
 export default App;
